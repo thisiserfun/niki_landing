a3688806e7eaebb4388a195244c8ebed160a08f3
Erfan Mhp <thisiserfun@gmail.com>
2025-08-07 05:52:39 +0330

    Initial commit

--- niki-portfolio/vite.config.js
+++ niki-portfolio/vite.config.js
@@ -4,4 +4,22 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    target: 'es2015',
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  },
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: false
+  }
 })
